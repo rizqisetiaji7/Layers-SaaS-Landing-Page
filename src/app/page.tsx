63f99b44e0a3dfe58ccb03d0eb43426026bda933
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2025-07-26 16:29:52 +0700

    Add integrations section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,17 @@ import Hero from "@/sections/Hero";
 import LogoTicker from "@/sections/LogoTicker";
 import Introduction from "@/sections/Introduction";
 import Features from "@/sections/Features";
+import Integrations from "@/sections/Integrations";
 
 export default function Home() {
-    return (
-        <>
-            <Navbar />
-            <Hero />
-            <LogoTicker />
-            <Introduction />
-            <Features />
-        </>
-    );
+   return (
+      <>
+         <Navbar />
+         <Hero />
+         <LogoTicker />
+         <Introduction />
+         <Features />
+         <Integrations />
+      </>
+   );
 }
