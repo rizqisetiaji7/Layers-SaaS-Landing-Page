0a7c4e89d365a6119894b37519f74c8023a112a1
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2025-07-26 19:06:00 +0700

    Import logo and change icon name

--- src/data/integrationsData.ts
+++ src/data/integrationsData.ts
@@ -1,32 +1,39 @@
+import figmaIcon from "@/assets/images/figma-logo.svg";
+import notionIcon from "@/assets/images/notion-logo.svg";
+import slackIcon from "@/assets/images/slack-logo.svg";
+import relumeIcon from "@/assets/images/relume-logo.svg";
+import framerIcon from "@/assets/images/framer-logo.svg";
+import githubIcon from "@/assets/images/github-logo.svg";
+
 const integrations = [
    {
       name: "Figma",
-      icon: "figma-logo.svg",
+      icon: figmaIcon,
       description: "Figma is a collaborative interface design tool.",
    },
    {
       name: "Notion",
-      icon: "notion-logo.svg",
+      icon: notionIcon,
       description: "Notion is an all-in-one workspace for notes and docs.",
    },
    {
       name: "Slack",
-      icon: "slack-logo.svg",
+      icon: slackIcon,
       description: "Slack is a powerful team communication platform.",
    },
    {
       name: "Relume",
-      icon: "relume-logo.svg",
+      icon: relumeIcon,
       description: "Relume is a no-code website builder and design system.",
    },
    {
       name: "Framer",
-      icon: "framer-logo.svg",
+      icon: framerIcon,
       description: "Framer is a professional website prototyping tool.",
    },
    {
       name: "GitHub",
-      icon: "github-logo.svg",
+      icon: githubIcon,
       description: "GitHub is the leading platform for code collaboration.",
    },
 ];
