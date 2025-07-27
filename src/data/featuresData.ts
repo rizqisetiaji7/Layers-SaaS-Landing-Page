e3d7c34d41d3a68b01574689b7c988b06f86c274
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2025-07-27 20:05:42 +0700

    Add featuresData

---
+++ src/data/featuresData.ts
@@ -0,0 +1,11 @@
+const features = [
+   { label: "Asset Library" },
+   { label: "Code Preview" },
+   { label: "Flow Mode" },
+   { label: "Smart Sync" },
+   { label: "Auto Layout" },
+   { label: "Fast Search" },
+   { label: "Smart Guides" },
+];
+
+export default features;
