cc6eaadddc3db35b41720785f2671af245d87373
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2025-07-27 09:22:47 +0700

    Add linksData.ts for navbarLinks and footerLinks

---
+++ src/data/linksData.ts
@@ -0,0 +1,14 @@
+const navLinks = [
+   { label: "Home", href: "#" },
+   { label: "Features", href: "#features" },
+   { label: "Integrations", href: "#integrations" },
+   { label: "FAQs", href: "#faqs" },
+];
+
+const footerLinks = [
+   { href: "#", label: "Contact" },
+   { href: "#", label: "Privacy Policy" },
+   { href: "#", label: "Terms & Conditions" },
+];
+
+export { navLinks, footerLinks };
